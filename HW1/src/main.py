f6528ba68c8be041a926187555ae00a895d027ac
ashwinramesh022 <117009209+ashwinramesh022@users.noreply.github.com>
2024-09-12 17:25:21 +0000

    test

--- HW1/src/main.py
+++ HW1/src/main.py
@@ -2,7 +2,7 @@
 main.py
 
 This module is the main entry point for the project. It contains the
-primary functionality and execution logic for the application.
+primary functionality and execution logic for the application..
 
 Functions:
 - add(): add 2 numbers.
