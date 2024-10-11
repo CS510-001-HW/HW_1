59a7a9e5e4436204f87fa4dae383abe049b7c2a9
Aditi105 <120356039+Aditi105@users.noreply.github.com>
2024-10-10 23:34:37 -0400

    Update task1.sh

--- HW4/task1.sh
+++ HW4/task1.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-
-pid=$(ps aux | grep '[i]nfinite.sh' | awk '{print $2}')
-kill "$pid"
-echo "Process infinite.sh with PID $pid has been killed."
+kill $(ps aux | grep '[i]nfinite.sh' | awk '{print $2}')
