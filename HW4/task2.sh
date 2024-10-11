4516f622b4ecb0c301087097d3a1e8fe2a68e4a0
ashwinramesh022 <117009209+ashwinramesh022@users.noreply.github.com>
2024-10-10 23:53:31 -0400

    Update task2.sh

--- HW4/task2.sh
+++ HW4/task2.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+#task2
 cd dataset1/ && grep -r -l "sample" | xargs grep -c "CSC510" | grep -E ":[3-9]$" | rev | sort -k1,1nr | rev | awk -F: '{print $1, $2}' | xargs -I{} sh -c 'file=$(echo "{}" | awk "{print \$1}"); count=$(echo "{}" | awk "{print \$2}"); size=$(ls -l "$file" | awk "{print \$5}"); echo "$file" "$count" "$size"' | sort -k2,2nr -k3,3nr | awk "{print \$1}" | sed 's/file_/filtered_/'
 
